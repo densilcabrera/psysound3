4fd586a64c602fb1333b92808e4e073f5b22799a
samuel.john.ferguson <samuel.john.ferguson@aa0e3d8e-bf9b-11dd-b9c8-e7e19bf6fede>
2009-06-30 07:51:58 +0000

    Documentation is heavy - should be split out.

---
+++ trunk/Objects/DataStorageObjects/@dataStorageArray/addSubjectiveData.m
@@ -0,0 +1,24 @@
+function addSubjectiveData(dObj,targetaudiofile)
+% ADDSUBJECTIVEDATA Add a subjective data file to the data tree
+%
+%
+% This code takes a formatted ps3 dataObject (usually a timeseries 
+% object and adds it to a special analyser folder called 
+% SubjectiveData. These objects behave in the same way as 
+% objective analysis data.
+%
+
+
+% Find targetaudio file node
+
+% Check if subjective data folder exists
+
+% Format data object into a dataObjS
+
+% Add Analyser Object (subclass from analyser)
+
+% alter dataInfo
+
+% Save
+
+
