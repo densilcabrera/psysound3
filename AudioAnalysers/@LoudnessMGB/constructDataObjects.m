d3070935f4181deaea4ca910663820b4796525ec
samuel.john.ferguson <samuel.john.ferguson@aa0e3d8e-bf9b-11dd-b9c8-e7e19bf6fede>
2011-11-09 22:22:51 +0000

    Make LoudnessMGB synchronise by using resampling.

--- AudioAnalysers/@LoudnessMGB/constructDataObjects.m
+++ AudioAnalysers/@LoudnessMGB/constructDataObjects.m
@@ -14,28 +14,48 @@ LongTermLoudness = dataBuffer{3};
 
 % Get the t data
 t = dataBuffer{4};
-oDataRate = diff(t);
-oDataRate = oDataRate(1);
+synch = get(obj,'synch');
+
+if synch
+  oDataRate = get(obj,'outputDataRate'); 
+  tResampled = [0:1/oDataRate:t(end)]';
+  tsI =  timeseries(InstantaneousLoudness,t);
+  tsI =  resample(tsI,tResampled);
+  tsL =  timeseries(LongTermLoudness,t);
+  tsL =  resample(tsL,tResampled);
+  tsS =  timeseries(ShortTermLoudness,t);
+  tsS =  resample(tsS,tResampled);
+  oDataRatet = diff(t);
+  oDataRatet = oDataRatet(1);
+  fprintf('Downsampling from %.1f Hz to %.1f Hz using Matlab timeseries resample command',...
+    1 / oDataRatet, 1 / oDataRate);
+else  
+  oDataRate = diff(t);
+  oDataRate = oDataRate(1);
+end
 
 
 % format for TimeSeries
-tsInst = createDataObject('tSeries',InstantaneousLoudness,t);
+tsInst = createDataObject('tSeries',tsI.Data,tsI.Time);
 tsInst.Name = 'Instantaneous Loudness';
 tsInst.DataInfo.Unit = 'Sones';
 tsInst.TimeInfo.Increment = oDataRate;
 output{1} = tsInst;
 
-tsShort = createDataObject('tSeries',ShortTermLoudness,t);
+tsShort = createDataObject('tSeries',tsS.Data,tsS.Time);
 tsShort.Name = 'Short-term Loudness';
 tsShort.DataInfo.Unit = 'Sones';
 tsShort.TimeInfo.Increment  = oDataRate;
 output{2} = tsShort;
 
-tsLong = createDataObject('tSeries',LongTermLoudness,t);
+tsLong = createDataObject('tSeries',tsL.Data,tsL.Time);
 tsLong.Name = 'Long-term Loudness';
 tsLong.DataInfo.Unit = 'Sones';
 tsLong.TimeInfo.Increment  = oDataRate;
 output{3} = tsLong;
+
+
+
 
 %Assign outputs to the object
 obj = set(obj, 'output', output);
